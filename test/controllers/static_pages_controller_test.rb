2f7859804361d69cab9f632f7b916adc415caeda
Leonard Bisson <leonardbisson@Leonards-iMac.fios-router.home>
2017-12-04 19:19:09 -0500

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Bayou Boss"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Need a Bigger Hat?"
+
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Bayou Boss"
+
   end
 
 end
